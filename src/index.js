0e2e453ddef2f3ec00281c4f4dd45d2c5081fb1f
Isaac Navarrete <navarreteisaac14@gmail.com>
2024-02-09 21:57:50 -0500

    added JSON formatting

--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
-
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+});
 
 app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m));
+    const result = Number(req.params.n) + Number(req.params.m);
+    res.json({ result });
 });
 
 app.get('/sub/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) - Number(req.params.m));
+    const result = Number(req.params.n) - Number(req.params.m);
+    res.json({ result });
 });
 
 app.get('/mult/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) * Number(req.params.m));
+    const result = Number(req.params.n) * Number(req.params.m);
+    res.json({ result });
 });
 
 app.get('/div/:n/:m', (req, res) => {
